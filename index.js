351878f3a407063241c25a5da5bafe47f029a943
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2024-05-19 13:14:56 -0400

    made read me

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const colors = require("colors");
-const path = require('path');
+const path = require("path");
 const { Rectangle, Circle, Triangle } = require("./shapes/shapes.js");
 
 // Above are all of the moduels I will be using for this challenge.
@@ -20,7 +20,7 @@ const questions = [
       return true;
     },
   },
-// Above, is a question to ask and make sure the input is a svg file type.
+  // Above, is a question to ask and make sure the input is a svg file type.
   {
     type: "input",
     name: "text",
@@ -34,10 +34,11 @@ const questions = [
   },
   // The question above checks if the input has a length greater than three or if it is empty.
   // Either of this occasions will result in a error message.
+
   {
     type: "input",
-    name: "color",
-    message: "Please enter a color or hexadecimal number:",
+    name: "textColor",
+    message: "Please enter a color or hexadecimal number for text color:",
     validate: function (input) {
       if (input.trim() === "") {
         return "Please enter a color or hexadecimal number!";
@@ -54,7 +55,7 @@ const questions = [
     },
   },
 
-  // The question above asks the user to input a color.
+  // The question above asks the user to input a text color.
   // The questions checks for a empty string
   // Also the question checks if the input passes the hex color test and color name test.
   // hexRegex and isColorName are regular expression tests.
@@ -66,123 +67,171 @@ const questions = [
     choices: ["Circle", "Triangle", "Square"],
   },
 
-  // Last we ask the user to pick a shape out of our shaoe array.
+  // Above, we ask the user to pick a shape out of our array.
+
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "Please enter a color or hexadecimal number for the background:",
+    validate: function (input) {
+      if (input.trim() === "") {
+        return "Please enter a color or hexadecimal number!";
+      }
+
+      const hexRegex = /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
+      const isColorName = /^[a-zA-Z]+$/;
+
+      if (!hexRegex.test(input) && !isColorName.test(input)) {
+        return "Please enter a valid hexadecimal color code!";
+      }
+
+      return true;
+    },
+  },
+
+  // Above we ask for th shape color.
 ];
 
 // Above, are the questions I will be using for inquirer
 
 function writeToFile(fileName, data) {
-
   const folderPath = path.join(__dirname, "examples");
   // I create a file path that links to the examples directory and store it in a variable.
 
   const filePath = path.join(folderPath, fileName);
- // I join the file path with the file name and store it in a variable .
+  // I join the file path with the file name and store it in a variable .
 
   fs.writeFile(filePath, data, (err) =>
     err ? console.error(err) : console.log("Success, your svg file is created!")
   );
-
-  
 }
 // Above, I created a function to write a file and pass the file path and data into it.
 
-
-
 function init() {
-  inquirer.prompt(questions).then((data) => {
-    let caseColor;
-    let shape;
-    let fileName = data.fileName
-    // Above we declare three variables, two of which we will  use for our switch statment.
-    const upperDataText = data.text.replace(/[a-zA-Z]/g, (letter) =>
-      letter.toUpperCase()
-    );
-    // Above, we make all letters in our text property uppercase.
-    const isColorName = /^[a-zA-Z]+$/.test(data.color);
-    const hexRegex = /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
-    const isHexColor = hexRegex.test(data.color);
-    // Above, we assign regualar expressions to variables that will be used for our switch statment.
-    switch (true) {
-      case isColorName:
-        caseColor = data.color.replace(/[A-Z]/g, (letter) =>
-          letter.toLowerCase()
-        );
+  inquirer
+    .prompt(questions)
+    .then((data) => {
+      let shapeColor;
+      let textColor;
+      let shape;
+      let fileName = data.fileName;
+      // Above we declare three variables, three  of which we will  use for our switch statments.
+      const upperDataText = data.text.replace(/[a-zA-Z]/g, (letter) =>
+        letter.toUpperCase()
+      );
+      // Above, we make all letters in our text property uppercase.
+      const isColorNameShape = /^[a-zA-Z]+$/.test(data.shapeColor);
+      const hexRegex = /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
+      const isHexColorShape = hexRegex.test(data.shapeColor);
+      const isColorNameText = /^[a-zA-Z]+$/.test(data.textColor);
+      const isHexColorText = hexRegex.test(data.textColor);
+      // Above, we assign regualar expressions to variables that will be used for our switch statments.
 
-        break;
-      case isHexColor:
-        caseColor = data.color;
+      switch (true) {
+        case isColorNameText:
+          textColor = data.textColor.replace(/[A-Z]/g, (letter) =>
+            letter.toLowerCase()
+          );
 
-        break;
-      default:
-        caseColor = "";
-    }
-    // Above, our first switch statment checks for our case of input for color.
-    // If a color is entered using letters then that color will be made to lower case.
-    // If a hex color is entered then we will use the hex color entered.
-    switch (data.shape) {
-      case "Circle":
-        shape = "circle";
-        break;
-      case "Triangle":
-        shape = "polygon";
+          break;
+        case isHexColorText:
+          textColor = data.textColor;
 
-        break;
+          break;
+        default:
+          textColor = "";
+      }
 
-      case "Square":
-        shape = "rect";
+      // Above is a switch statment for the text color being letters or hex#.
 
-        break;
+      switch (true) {
+        case isColorNameShape:
+          shapeColor = data.shapeColor.replace(/[A-Z]/g, (letter) =>
+            letter.toLowerCase()
+          );
 
-      default:
-        shape = " ";
-    }
+          break;
+        case isHexColorShape:
+          shapeColor = data.shapeColor;
 
-    //Above, our second switch statment checks for the input of shape we recieved.
-    // Then the shape for a svg file is stored in the shape variable.
-  
+          break;
+        default:
+          shapeColor = "";
+      }
+      // Above, our second switch statment checks for our case of input for shapeColor.
+      // If a color is entered using letters then that color will be made to lower case.
+      // If a hex color is entered then we will use the hex color entered.
 
-    return { fileName, caseColor, shape, upperDataText };
-    // Above, I return these values to use in the next .then.
-  })
-  .then(results => {
-    let newShapeObj;
+      switch (data.shape) {
+        case "Circle":
+          shape = "circle";
+          break;
+        case "Triangle":
+          shape = "polygon";
 
-    // Above I initalize a the variable newShapeObj.
+          break;
 
+        case "Square":
+          shape = "rect";
 
-    switch (results.shape) {
-      case "circle":
-        newShapeObj = new Circle(results.caseColor,results.upperDataText);
-        break;
+          break;
 
-      case "polygon":
-        newShapeObj = new Triangle(results.caseColor,results.upperDataText);
-        break;
+        default:
+          shape = " ";
+      }
 
-      case "rect":
-        newShapeObj = new Rectangle(results.caseColor, results.upperDataText);
-        break;
+      //Above, our third switch statment checks for the input of shape we recieved.
+      // Then the shape for a svg file is stored in the shape variable.
 
-      default:
-        newShapeObj = " ";
-    };
+      return { fileName, shapeColor, shape, upperDataText, textColor };
+      // Above, I return these values to use in the next .then.
+    })
+    .then((results) => {
+      let newShapeObj;
 
-    console.log(newShapeObj)
+      // Above I initalize a the variable newShapeObj.
 
-    //Above, is a switch statment that creates a new shape object depending on the value of shape.
+      switch (results.shape) {
+        case "circle":
+          newShapeObj = new Circle(
+            results.shapeColor,
+            results.upperDataText,
+            results.textColor
+          );
+          break;
 
-    writeToFile(results.fileName,newShapeObj.markUP());
+        case "polygon":
+          newShapeObj = new Triangle(
+            results.shapeColor,
+            results.upperDataText,
+            results.textColor
+          );
+          break;
 
-    //Above, I use my write to file function and pass in the filename and newShapeObj function markUP.
+        case "rect":
+          newShapeObj = new Rectangle(
+            results.shapeColor,
+            results.upperDataText,
+            results.textColor
+          );
+          break;
 
+        default:
+          newShapeObj = " ";
+      }
 
+      console.log(newShapeObj);
 
-  })
+      //Above, is a switch statment that creates a new shape object depending on the value of shape.
 
-  .catch((err) => {
-    console.error('Error writing to file:', err);
-  });
+      writeToFile(results.fileName, newShapeObj.markUP());
+
+      //Above, I use my write to file function and pass in the filename and newShapeObj function markUP.
+    })
+
+    .catch((err) => {
+      console.error("Error writing to file:", err);
+    });
 
   // Above, is the catch function to end the chain if .then's.
 }
--- shapes/shapes.js
+++ shapes/shapes.js
@@ -1,34 +1,33 @@
 class Shape {
-  constructor(color, text) {
+  constructor(shapeColor, text, textColor) {
     if (text.length > 3) {
       throw new Error("Invalid text length!");
     }
 
     this.text = text;
-    this.color = color;
+    this.shapeColor = shapeColor;
+    this.textColor = textColor;
   }
-};
+}
 // Above, I create a parent class constructor named shape
-// This constructor takes in color and text and checks if the text length is greater than 3.
+// This constructor takes in shapeColor text and textColor and checks if the text length is greater than 3.
 
 class Rectangle extends Shape {
-  constructor(color, text) {
-    super(color, text);
+  constructor(shapeColor, text, textColor) {
+    super(shapeColor, text, textColor);
   }
-};
+}
 
 // Above, I create a child class constructor for rectangle shapes.
 // This constructor inherits the properties of Shape.
 // The same is repeated for the folowing shapes.
 
 Rectangle.prototype.markUP = function () {
-
-
   return `
   <svg width="500" height="500" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <rect x="10" y="10" width="270" height="150" fill="${this.color}" />
+    <rect x="10" y="10" width="270" height="150" fill="${this.shapeColor}" />
     <text x="145" y="85" text-anchor="middle" alignment-baseline="middle" font-family="Arial" font-size="97"
-        fill="white">
+        fill="${this.textColor}">
         ${this.text}
     </text>
 
@@ -38,7 +37,7 @@ Rectangle.prototype.markUP = function () {
 };
 
 Rectangle.prototype.render = function () {
-    return `<rect x="10" y="10" width="270" height="150" fill="${this.color}"/>`
+  return `<rect x="10" y="10" width="270" height="150" fill="${this.shapeColor}"/>`;
 };
 
 //Above, I a add two functions called render and markup.
@@ -47,20 +46,17 @@ Rectangle.prototype.render = function () {
 // The same is repeated for the folowing shapes.
 
 class Circle extends Shape {
-  constructor(color, text) {
-    super(color, text);
+  constructor(shapeColor, text, textColor) {
+    super(shapeColor, text, textColor);
   }
 }
 
 Circle.prototype.markUP = function () {
-
-  
-
   return `
     
     <svg width="500" height="500" version="1.1" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="170" cy="170" r="150" fill="${this.color}" />
-      <text x="170" y="170" text-anchor="middle" alignment-baseline="middle" font-family="Arial" font-size="95" fill="white">
+      <circle cx="170" cy="170" r="150" fill="${this.shapeColor}" />
+      <text x="170" y="170" text-anchor="middle" alignment-baseline="middle" font-family="Arial" font-size="95" fill="${this.textColor}">
         ${this.text}
       </text>
     </svg>
@@ -69,35 +65,30 @@ Circle.prototype.markUP = function () {
 };
 
 Circle.prototype.render = function () {
-    return `<circle cx="170" cy="170" r="150" fill="${this.color}"/>`
+  return `<circle cx="170" cy="170" r="150" fill="${this.shapeColor}"/>`;
 };
-
 
 class Triangle extends Shape {
-  constructor(color, text) {
-    super(color, text);
+  constructor(shapeColor, text, textColor) {
+    super(shapeColor, text, textColor);
   }
-};
+}
 
 Triangle.prototype.markUP = function () {
-
-
   return `
   <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-    <polygon points="150,30 270,270 30,270" fill="${this.color}"/>
-    <text x="150" y="200" text-anchor="middle" font-family="Arial" font-size="45" fill="white">${this.text}</text>
+    <polygon points="150,30 270,270 30,270" fill="${this.shapeColor}"/>
+    <text x="150" y="200" text-anchor="middle" font-family="Arial" font-size="45" fill="${this.textColor}">${this.text}</text>
   </svg>
    
     
     `;
 };
 
-Triangle.prototype.render= function () {
-    return `<polygon points="150,30 270,270 30,270" fill="${this.color}"/>`
+Triangle.prototype.render = function () {
+  return `<polygon points="150,30 270,270 30,270" fill="${this.shapeColor}"/>`;
 };
-
-
 
 module.exports = { Rectangle, Circle, Triangle };
 
-// Above, I export all the classes in a object. 
+// Above, I export all the classes in a object.
